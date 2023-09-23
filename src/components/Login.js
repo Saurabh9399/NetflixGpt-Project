5c64dc09c5792890cbb12409c8deccc4372c9a9d
Saurabh Jirapure <saurabhj8087445@gmail.com>
2023-09-24 00:43:31 +0530

    created constants file

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,9 +7,9 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { LOGIN_BACKGROUND_IMAGE, USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const email = useRef(null);
@@ -17,7 +17,6 @@ const Login = () => {
   const password = useRef(null);
   const [errMessage, setErrMessage] = useState();
   const [isSignIn, setIsSignIn] = useState(true);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleSigInSignUpToggle = () => {
@@ -45,7 +44,7 @@ const Login = () => {
 
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://avatars.githubusercontent.com/u/64205497?v=4",
+            photoURL: USER_AVATAR,
           })
             .then(() => {
               const { uid, email, displayName, photoURL } = auth.currentUser;
@@ -94,7 +93,7 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f85718e8-fc6d-4954-bca0-f5eaf78e0842/ea44b42b-ba19-4f35-ad27-45090e34a897/IN-en-20230918-popsignuptwoweeks-perspective_alpha_website_large.jpg" />
+        <img src={LOGIN_BACKGROUND_IMAGE} />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
